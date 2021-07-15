aa93ac719650415261a86de4be0c29ab9320fb2b
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-15 14:16:04 +0900

    リストの表示

--- app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
@@ -2,10 +2,48 @@ package com.websarva.wings.android.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menuList:MutableList<MutableMap<String,String>> = mutableListOf()
+        var menu = mutableMapOf("name" to "から揚げ定食","price" to "800円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "生姜焼き定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ステーキ定食", "price" to "1000円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "野菜炒め定食", "price" to "750円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "とんかつ定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ミンチかつ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "チキンカツ定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "コロッケ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "回鍋肉定食", "price" to "750円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "麻婆豆腐定食", "price" to "800円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "青椒肉絲定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼肉定食", "price" to "950円")
+        menuList.add(menu)
+
+        val from = arrayOf("name","price")
+        val to = intArrayOf(android.R.id.text1,android.R.id.text2)
+        val adapter = SimpleAdapter(this@MainActivity,menuList,android.R.layout.simple_list_item_2,from,to)
+        lvMenu.adapter = adapter
     }
 }
